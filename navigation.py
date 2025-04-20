aa198676d503731fc40af753f619a5f59ef805ba
varunchopra11 <varunchopra2003@gmail.com>
2025-04-20 10:35:02 +0530

    Remove file logging from the navigation module to streamline logging output

--- navigation.py
+++ navigation.py
@@ -18,7 +18,6 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
     handlers=[
-        logging.FileHandler("robot_navigation.log"),
         logging.StreamHandler()
     ]
 )
